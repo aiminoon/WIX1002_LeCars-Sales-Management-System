219ca3e1df300a9f28514bcd8041764070ad9c3d
imranmy4 <149666023+imranmy4@users.noreply.github.com>
2023-12-28 15:11:58 +0800

    Update EnterCustomerData.java

--- src/main/java/com/mycompany/app/EnterCustomerData.java
+++ src/main/java/com/mycompany/app/EnterCustomerData.java
@@ -1,4 +1,4 @@
-package com.mycompany.app;
+package intlocjava.lecars_project;
 
 import java.io.FileWriter;
 import java.io.PrintWriter;
@@ -10,7 +10,7 @@ import java.util.Scanner;
 
 public class EnterCustomerData {
     
-    public static void customerData(){
+    public static void customerData(int checkES){
         Scanner input = new Scanner(System.in);
         String customerName;
         String phoneNumber;
@@ -21,7 +21,7 @@ public class EnterCustomerData {
         phoneNumber = input.nextLine();
         System.out.print("Please enter customer postcode: ");
         postcode = input.nextLine();
-        int rowsCount = -1;
+        int rowsCount = -1, rowsCountOwn = 0;
         try (LineNumberReader lnr = new LineNumberReader(new FileReader("cust.csv"))) {
             lnr.skip(Long.MAX_VALUE);
             rowsCount = lnr.getLineNumber() - 1;
@@ -59,5 +59,59 @@ public class EnterCustomerData {
         catch(IOException e){
             e.printStackTrace();
         }
-    } 
+        
+        if(checkES == 0){
+            
+            String employeeID;
+            System.out.print("Enter your employee ID: ");
+            employeeID = input.nextLine();
+            try (FileWriter fw = new FileWriter(employeeID + ".csv", true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter pw = new PrintWriter(bw)) {
+                try (LineNumberReader lineNum = new LineNumberReader(new FileReader(employeeID + ".csv"))) {
+                lineNum.skip(Long.MAX_VALUE);
+                rowsCountOwn = lineNum.getLineNumber();
+                System.out.println("Number of rows in the file: " + rowsCountOwn);
+                if(rowsCountOwn == 0){
+                    pw.print("Customer ID");
+                    pw.print(",");
+                    pw.print("Customer Name");
+                    pw.print(",");
+                    pw.print("Phone Number");
+                    pw.print(",");
+                    pw.print("Postcode");
+                    pw.println();
+                    
+                }
+                } catch (IOException e) {
+                    System.out.println("An error occured while counting number of rows.");
+                    e.printStackTrace();
+                }
+            
+                String customerId = null;
+
+                if(rowsCount>=999){
+                    customerId="C" + (rowsCount+1);
+                } else if(rowsCount>=99) {
+                    customerId="C0" + (rowsCount+1);
+                } else if (rowsCount>=9) {
+                    customerId="C00" + (rowsCount+1);
+                } else if (rowsCount>=0){
+                    customerId="C000" + (rowsCount+1); 
+                }
+
+                pw.print(customerId);
+                pw.print(",");
+                pw.print(customerName);
+                pw.print(",");
+                pw.print(phoneNumber);
+                pw.print(",");
+                pw.print(postcode);
+                pw.println();
+            }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+        }
+    }
 }
