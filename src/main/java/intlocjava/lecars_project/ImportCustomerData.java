045bf4765e0f95839ef1973e33d3d748cc276232
Han <98066859+cris-ict-Logan@users.noreply.github.com>
2024-01-07 11:39:12 +0700

    Update ImportCustomerData.java

--- src/main/java/intlocjava/lecars_project/ImportCustomerData.java
+++ src/main/java/intlocjava/lecars_project/ImportCustomerData.java
@@ -44,30 +44,37 @@ private String custId;
 }
 
 public class ImportCustomerData {
-    public static void main(String[] args) {
+    // Remove the comments on the main method to import the files by running the ImportCustomerData class itself
+    /*public static void main(String[] args) {
             String currentDir = System.getProperty("user.dir");
-        String filePath = currentDir + File.separator + "cust.csv";
+        String filePath = currentDir + File.separator + "newcust.csv";
         
         System.out.println(filePath);
         
         ImportCustomerData.importCustomers();
-    }
+    }*/
    
     
     public static void importCustomers(){
         try {
             BufferedReader br = new BufferedReader(new FileReader("cust.csv"));
+            BufferedWriter bw = new BufferedWriter(new FileWriter("newcust.csv"));
+            
             String line;
             while ((line = br.readLine()) != null){
                 String[] data = line.split(",");
                 Customer cust = new Customer(data[0], data[1], data[2], data[3]);
                 
+                bw.write(line);
+                bw.newLine();
+                
             }
             br.close();
+            bw.close();
         
         } catch (IOException e){
             e.printStackTrace();
-            }
+        }
     }
 }
   
