c0bb5acb1550d114fc855f534b4a3ddfcaae48d5
aiminoon <149665483+aiminoon@users.noreply.github.com>
2024-01-10 21:36:39 +0800

    Update ImportVehicleData.java

--- src/main/java/intlocjava/lecars_project/ImportVehicleData.java
+++ src/main/java/intlocjava/lecars_project/ImportVehicleData.java
@@ -2,61 +2,15 @@ package intlocjava.lecars_project;
 
 import java.io.*; 
 
-class Vehicles {
-    private String carPlate;
-    private String carModel;
-    private int acquirePrice;
-    private int carStatus;
-    private int salesPrice;
-    
-    public Vehicles(String plate, String mod, int acPri, int stat, int salePri){
-        this.carPlate = plate;
-        this.carModel = mod;
-        this.acquirePrice = acPri;
-        this.carStatus = stat;
-        this.salesPrice = salePri;
-    }
-    
-    public String getCarPlate() {
-        return carPlate;
-    }
-    
-    public String getModel() {
-        return carModel;
-    }
-    
-    public int getAcPrice() {
-        return acquirePrice;
-    }
-    
-    public int getCarStatus() {
-        return carStatus;
-    }
-    
-    public int getSalesPrice() {
-        return salesPrice; 
-    }
-    
-    @Override
-    public String toString(){
-        return "Car Plate: " + carPlate + ", Car Model: " + carModel + ", Acquire Price: " + acquirePrice + ", Car Status: " + carStatus + ", Sales Price: " + salesPrice; 
-    }
-
-}
-
 public class ImportVehicleData {
-    public static void main(String[] args) {        
-        ImportVehicleData.importAndExportVehicle();
-    }
-    
-    private static List<Employees> employees = new ArrayList<>();
     
     public static String getFilePath(){
         String currentDir = System.getProperty("user.dir");
+        
         return currentDir + File.separator + "vehicle.csv";
     }
 
-    public static void importAndExportVehicle() {
+    public static void importVehicle() {
         System.out.println(getFilePath());
         
         String dir = System.getProperty("user.home");
@@ -65,21 +19,18 @@ public class ImportVehicleData {
         try (BufferedReader br = new BufferedReader(new FileReader(path));
              BufferedWriter bw = new BufferedWriter(new FileWriter("vehicle.csv"))) {
 
-            br.readLine(); // Skip the header line (assuming 1st line is a header)
             String line;
+            
             while ((line = br.readLine()) != null) {
-                String[] data = line.split(",");
-                if (data.length >= 5) {
-                    Vehicles vehicle = new Vehicles(data[0], data[1], Integer.parseInt(data[2]), Integer.parseInt(data[3]), Integer.parseInt(data[4]));
-                    String csvRepresentation = vehicle.toString(); // or use a toCSVFormat method if you have one
-                    bw.write(csvRepresentation);
-                    bw.newLine();
-                } else {
-                    System.out.println("Incomplete data for a vehicle: " + line);
-                }
+                bw.write(line);
+                bw.newLine();
             }
-
-        } catch (IOException | NumberFormatException e) {
+            
+            br.close();
+            bw.close();
+            
+        } catch (IOException e) {
+            System.out.println("An error occured when importing vehicle data from vehicle.csv");
             e.printStackTrace();
         }
     }
