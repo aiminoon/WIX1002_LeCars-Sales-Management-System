04bd87a0d3e2780f09cc7c532f8de58adaf77bff
aiminoon <149665483+aiminoon@users.noreply.github.com>
2024-01-10 23:54:24 +0800

    Update ImportSalesData.java

--- src/main/java/intlocjava/lecars_project/ImportSalesData.java
+++ src/main/java/intlocjava/lecars_project/ImportSalesData.java
@@ -28,6 +28,17 @@ public class ImportSalesData {
 
             br.close();
             bw.close();
+
+            try (LineNumberReader lnr = new LineNumberReader(new FileReader("sales.csv"))) {
+                lnr.skip(Long.MAX_VALUE);
+                int rowsCount = -1;
+                rowsCount = lnr.getLineNumber();
+                System.out.println("Number of rows in sales.csv file is " + rowsCount);
+                
+            } catch (IOException e) {
+                System.out.println("An error occured while counting number of rows in sales.csv file.");
+                e.printStackTrace();
+            }
             
         } catch (IOException e) {
             System.out.println("An error occured when importing sales data from sales.csv");
