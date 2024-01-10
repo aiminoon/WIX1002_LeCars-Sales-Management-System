230bb6a5bafca44c00b52ef85746984d57f9106f
aiminoon <149665483+aiminoon@users.noreply.github.com>
2024-01-10 21:05:58 +0800

    Update EnterSalesData.java

--- src/main/java/intlocjava/lecars_project/EnterSalesData.java
+++ src/main/java/intlocjava/lecars_project/EnterSalesData.java
@@ -11,6 +11,7 @@ import java.util.Date;
 import java.util.Scanner;
 
 public class EnterSalesData {
+    
     public void SalesData(String employeeId, int checkES) {
         Scanner scanner = new Scanner(System.in);
 
@@ -23,7 +24,30 @@ public class EnterSalesData {
         try (FileWriter fw = new FileWriter("sales.csv", true);
             BufferedWriter bw = new BufferedWriter(fw);
             PrintWriter pw = new PrintWriter(bw)) {
-                    
+            
+            try (LineNumberReader lnr = new LineNumberReader(new FileReader("sales.csv"))) {
+                lnr.skip(Long.MAX_VALUE);
+                int rowsCount = -1;
+                rowsCount = lnr.getLineNumber();
+
+                if(rowsCount == 0){
+                   pw.print("salesId");
+                   pw.print(",");
+                   pw.print("dateTime");
+                   pw.print(",");
+                   pw.print("carPlate");
+                   pw.print(",");
+                   pw.print("custId");
+                   pw.print(",");
+                   pw.print("employeeId");
+                   pw.println();
+                }
+                
+            } catch (IOException e) {
+                System.out.println("An error occured while printing first row in sales.csv file.");
+                e.printStackTrace();
+            }
+            
             pw.print(getSalesId());
             pw.print(",");
             pw.print(getCurrentDateTime());
@@ -33,61 +57,19 @@ public class EnterSalesData {
             pw.print(custId);
             pw.print(",");
             pw.print(employeeId);
-
-            System.out.println("Successfully saved the sales data in Sales.csv file.");
+            pw.println();
+            
+            System.out.println("Successfully saved the sales data in sales.csv file.");
 
         } catch (IOException e) {
-            System.out.println("An error occurred while storing sales data in Sales.csv file.");
+            System.out.println("An error occurred while storing sales data in sales.csv file.");
             e.printStackTrace();
         }
-        
-        if(checkES == 0){
-            try (FileWriter fw = new FileWriter(employeeId + "sales.csv", true);
-            BufferedWriter bw = new BufferedWriter(fw);
-            PrintWriter pw = new PrintWriter(bw)) {
-                try (LineNumberReader lineNum = new LineNumberReader(new FileReader(employeeId + "sales.csv"))) {
-                    
-                lineNum.skip(Long.MAX_VALUE);
-                int rowsCountOwn = lineNum.getLineNumber();
-                System.out.println("Number of rows in the " + employeeId + "sales.csv file: " + rowsCountOwn);
-                
-                if(rowsCountOwn == 0){
-                    pw.print("salesId");
-                    pw.print(",");
-                    pw.print("dateTime");
-                    pw.print(",");
-                    pw.print("carPlate");
-                    pw.print(",");
-                    pw.print("custId");
-                    pw.println();
-                    pw.print("employeeId");
-                    pw.println();
-                }
-                } catch (IOException e) {
-                    System.out.println("An error occured while counting number of rows in the " + employeeId + "sales.csv file.");
-                    e.printStackTrace();
-                }
-
-                pw.print(getSalesId());
-                pw.print(",");
-                pw.print(getCurrentDateTime());
-                pw.print(",");
-                pw.print(carPlate);
-                pw.print(",");
-                pw.print(custId);
-                pw.print(",");
-                pw.print(employeeId);
-            }
-            catch(IOException e){
-                System.out.println("An error occured while entering new data in the " + employeeId + "sales.csv file.");
-                e.printStackTrace();
-            }
-        }
-    scanner.close();
     }
        
     private String getCurrentDateTime() {
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        
         return df.format(new Date());
     }
         
@@ -97,22 +79,28 @@ public class EnterSalesData {
 
         try (LineNumberReader lnr = new LineNumberReader(new FileReader("sales.csv"))) {
             lnr.skip(Long.MAX_VALUE);
-            rowsCount = lnr.getLineNumber() - 1;
-            System.out.println("Number of rows in the Sales.csv file: " + rowsCount);
-            if(rowsCount>999){
-                salesId="A" + (rowsCount+1);
-            } else if(rowsCount>99) {
-                salesId="A0" + (rowsCount+1);
-            } else if(rowsCount>9) {
-                salesId="A00" + (rowsCount+1);
-            } else if(rowsCount>=0) {
-                salesId="A000" + (rowsCount+1);
-            } 
+            rowsCount = lnr.getLineNumber();
+            System.out.println("Number of rows in the sales.csv file before adding new data: " + rowsCount);
+            
+            if(rowsCount > 999){
+                salesId = "A" + (rowsCount);
+            } else if(rowsCount > 99) {
+                salesId = "A0" + (rowsCount);
+            } else if(rowsCount > 9) {
+                salesId = "A00" + (rowsCount);
+            } else if(rowsCount > 0) {
+                salesId = "A000" + (rowsCount);
+            } else if(rowsCount == 0) {
+                salesId = "A000" + (rowsCount + 1);
+            } else {
+                System.out.println("An error occured while generating salesId in sales.csv file."); 
+            }
+            
         } catch (IOException e) {
-            System.out.println("An error occured while counting number of rows in Sales.csv file.");
+            System.out.println("An error occured while counting number of rows in sales.csv file.");
             e.printStackTrace();
         }
+        
         return salesId;
     }
-    
 }
