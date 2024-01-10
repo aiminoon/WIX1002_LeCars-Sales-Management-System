7f0a328d220a40dafd5f26bf72691120761131c8
aiminoon <149665483+aiminoon@users.noreply.github.com>
2024-01-10 21:12:01 +0800

    Update ViewAllVehicle.java

--- src/main/java/intlocjava/lecars_project/ViewAllVehicle.java
+++ src/main/java/intlocjava/lecars_project/ViewAllVehicle.java
@@ -6,27 +6,24 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- *
- * @author User
- */
 public class ViewAllVehicle {
+    
     public static void displayVehicles(){
         List<String[]> vehicleData = new ArrayList<>();
         
         try {
             BufferedReader br = new BufferedReader(new FileReader("vehicle.csv"));
             String line;
+            
             while((line = br.readLine()) != null)  {
-                String[] row = line.split(","); //split CSV line by comma
+                String[] row = line.split(","); 
                 vehicleData.add(row);
             }
-            
-            // Display sales records for the given employee ID
-            System.out.println("All vehicle recoreds: ");
+
+            System.out.println("All vehicle records: ");
             
             for (String[] row : vehicleData){
-                if(row.length >= 5) { //ensure there are at least 5 elements in row array
+                if(row.length == 5) { 
                     String carPlate= row[0];
                     String carModel = row[1];
                     String acquirePrice = row[2];
@@ -37,14 +34,28 @@ public class ViewAllVehicle {
                         carStatus = "Out of Stock"; 
                     } 
                     
-                    System.out.printf("%-10s   %-30s    %-14s    %-15s\n", carPlate, carModel, acquirePrice, carStatus, salesPrice);
-             
-                } else {
-                    // Handle rows with null data 
-                    System.out.println("In Stock: " + String.join(", ", row));
+                    System.out.printf("%-10s   %-30s    %-14s    %-15s  %-10s \n", 
+                            carPlate, carModel, acquirePrice, carStatus, salesPrice);
+                }
+                
+                if(row.length == 4) { 
+                    String carPlate= row[0];
+                    String carModel = row[1];
+                    String acquirePrice = row[2];
+                    String carStatus = row[3];
+                    String salesPrice = "";
+                    
+                    if(carStatus.equals("1")){
+                        carStatus = "In Stock"; 
+                    } 
+
+                    System.out.printf("%-10s   %-30s    %-14s    %-15s  %-10s \n", 
+                            carPlate, carModel, acquirePrice, carStatus, salesPrice);
                 }
             }
+            
         } catch (IOException | NumberFormatException e){
+            System.out.println("An error occured when priting data from vehicle.csv file.");
             e.printStackTrace();  
         }
     }
