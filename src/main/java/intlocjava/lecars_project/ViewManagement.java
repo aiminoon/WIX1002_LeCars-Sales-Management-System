6334f275caf5823e3690311d1a770ec75ec6c977
aiminoon <149665483+aiminoon@users.noreply.github.com>
2024-01-10 21:12:26 +0800

    Update ViewManagement.java

--- src/main/java/intlocjava/lecars_project/ViewManagement.java
+++ src/main/java/intlocjava/lecars_project/ViewManagement.java
@@ -3,6 +3,7 @@ package intlocjava.lecars_project;
 import java.io.*;
 
 public class ViewManagement {
+    
     public static void viewAllInfo(String filename) {
         try {
             BufferedReader br = new BufferedReader(new FileReader(filename));
@@ -12,20 +13,27 @@ public class ViewManagement {
             while ((line = br.readLine()) != null){
                 String[] data = line.split(",");
                 
-                printInfo(data);
+                if (i == 0) {
+                    printInfo(data);
+                    i++;
+                } else {
+                    printInfo(data);
+                }
             }
         
         } catch (IOException e){
+            System.out.println("An error occured while reading " + filename + ".");
             e.printStackTrace();
         }
     }
 
     public static void printInfo(String[] data) {
         if (data.length == 4) {
-            System.out.printf("%-10s   %-30s    %-14s    %-15s\n", data[0], data[1], data[2], data[3]);
+            System.out.printf("%-10s   %-30s    %-14s    %-15s\n", 
+                    data[0], data[1], data[2], data[3]);
         } else {
-            System.out.printf("%-10s   %-30s    %-14s    %-15s   %-15s\n", data[0], data[1], data[2], data[3], data[4]);
+            System.out.printf("%-10s   %-30s    %-14s    %-15s   %-15s\n",
+                    data[0], data[1], data[2], data[3], data[4]);
         }
     }
-
 }
