e06e012d3d14a380ff5621e470afe95ef526d817
aiminoon <149665483+aiminoon@users.noreply.github.com>
2024-01-10 21:07:41 +0800

    Update Login.java

--- src/main/java/intlocjava/lecars_project/Login.java
+++ src/main/java/intlocjava/lecars_project/Login.java
@@ -6,14 +6,18 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Login {
-    public static int Login() {
+    
+    public int Login() {
+        
         int employeeStatus = -1;
         String line = "";
         String splitBy = ",";
+        
         Scanner input = new Scanner(System.in);
         
         System.out.println("Enter employee name:");
         String name = input.nextLine();
+        
         System.out.println("Enter password:");
         String password = input.nextLine();
         
@@ -23,6 +27,7 @@ public class Login {
 
             while ((line = br.readLine()) != null) {
                 String[] employee = line.split(splitBy);
+                
                 if (name.equals(employee[1]) && password.equals(employee[3])) {
                     employeeStatus = Integer.valueOf(employee[2]);
                     System.out.println("Welcome " + name + "!\n");
@@ -35,10 +40,12 @@ public class Login {
                 System.out.println("Wrong employee name or password!");
                 System.out.println("Please try again!\n");
             }
+            
         } catch (IOException e) {
             System.out.println("An error occurred while login.");
             e.printStackTrace();
         }
+        
         return employeeStatus;
     }
 }
