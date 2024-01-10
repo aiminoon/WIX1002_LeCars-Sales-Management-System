b37fa86888ef78d5a1fec0ca612b9b530a2a1d69
aiminoon <149665483+aiminoon@users.noreply.github.com>
2024-01-10 21:02:20 +0800

    Update Bonus.java

--- src/main/java/intlocjava/lecars_project/Bonus.java
+++ src/main/java/intlocjava/lecars_project/Bonus.java
@@ -15,7 +15,7 @@ public class Bonus {
         double salesAmount = getSalesAmount(ID);
 
         if (salesCount > 15 || salesAmount > 1000000) {
-            bonus = 500;
+            bonus = 500;  
         } else {
             System.out.println("You failed to have more than 15 car sales records,"
                                + " or more than RM1 million sales amount within a calendar month. "
@@ -36,7 +36,7 @@ public class Bonus {
         } else if (salesAmount >= 800000.01) {
             bonus = salesAmount * (1.15/100);
         } else if (salesAmount <= 800000.00) {
-            bonus = salesAmount * (1.25/100);
+            bonus = salesAmount * (1.00/100);
         } else {
             System.out.println("Error in calculating management employee bonus.");
         }
@@ -49,16 +49,17 @@ public class Bonus {
 
         try (BufferedReader br = new BufferedReader(new FileReader("sales.csv"))) {
             String line;
-            br.readLine(); // Skip the header line
+            br.readLine();
 
             while ((line = br.readLine()) != null) {
                 String[] sales = line.split(",");
+                
                 if (ID.equals(sales[4])) {
                     salesCount++;
                 }
             }
         } catch (IOException e) {
-            System.out.println("An error occured when counting sales records.");
+            System.out.println("An error occured when counting sales records in sales.csv.");
             e.printStackTrace();
         }
 
@@ -66,26 +67,31 @@ public class Bonus {
     }
     
     private double getSalesAmount(String ID) {
-        String employeeID = ID;
-        String line = "";
-        String splitBy = ",";
-        Scanner input = new Scanner(System.in);
-        LocalDateTime date = LocalDateTime.now();
-        int currentMonth = date.getMonthValue();
-        String[] carPlates = new String[10];
         int i = 0;
         double salesAmount = 0;
         
+        String employeeID = ID;
+        String line = "";
+        String splitBy = ",";
+        String[] carPlates = new String[10];
+        
+        Scanner input = new Scanner(System.in);
+        
+        LocalDateTime date = LocalDateTime.now();
+        int currentMonth = date.getMonthValue();
+
         try {
             BufferedReader br = new BufferedReader(new FileReader("sales.csv"));
             boolean found = false;
 
-            br.readLine();//skip first line
+            br.readLine();
             while ((line = br.readLine()) != null) {
                 String[] sales = line.split(splitBy);
                 String currentTimeDate = sales[1];
+                
                 LocalDateTime dateCheck = LocalDateTime.parse(currentTimeDate, DateTimeFormatter.ISO_DATE_TIME);
                 int monthCheck = dateCheck.getMonthValue();
+                
                 if (employeeID.equals(sales[4]) && monthCheck == currentMonth) {
                     carPlates[i] = sales[2];
                     i++;
@@ -96,30 +102,32 @@ public class Bonus {
             if (!found) {
                 System.out.println("No sales has been made this month");
             }
+            
         } catch (IOException e) {
             System.out.println("An error occurred while searching car plate in sales.csv");
             e.printStackTrace();
         }
         
         if(carPlates != null){
-        try {
-            BufferedReader br = new BufferedReader(new FileReader("vehicle.csv"));
+            try {
+                BufferedReader br = new BufferedReader(new FileReader("vehicle.csv"));
 
-            br.readLine();//skip first line
-            while ((line = br.readLine()) != null) {
-                String[] vehicles = line.split(splitBy);
-                for(int j = 0; j<carPlates.length; j++){
-                    if(vehicles[0].equals(carPlates[j])){
-                        salesAmount += Double.parseDouble(vehicles[4]);
-                        break;
+                br.readLine();
+                while ((line = br.readLine()) != null) {
+                    String[] vehicles = line.split(splitBy);
+                    
+                    for(int j = 0; j < carPlates.length; j++){
+                        if(vehicles[0].equals(carPlates[j])){
+                            salesAmount += Double.parseDouble(vehicles[4]);
+                            break;
+                        }
                     }
                 }
-            }
 
-        } catch (IOException e) {
-            System.out.println("An error occurred while counting sales amount.");
-            e.printStackTrace();
-        }
+            } catch (IOException e) {
+                System.out.println("An error occurred while counting sales amount.");
+                e.printStackTrace();
+            }
         }
         return salesAmount;
     }
