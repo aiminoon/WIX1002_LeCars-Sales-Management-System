73d1e01373cc175b748bbfbbcf3c675c656b5626
aiminoon <149665483+aiminoon@users.noreply.github.com>
2024-01-10 21:12:58 +0800

    Update ViewProfitMargin.java

--- src/main/java/intlocjava/lecars_project/ViewProfitMargin.java
+++ src/main/java/intlocjava/lecars_project/ViewProfitMargin.java
@@ -13,29 +13,33 @@ public class ViewProfitMargin {
     public static double calculateProfitMargin() {
         int totalAquirePrice = 0, totalSalesPrice = 0;
         double totalSalaryEmployee = 0;
+        
         try {
-            
             BufferedReader br = new BufferedReader(new FileReader("sales.csv"));
+            
             String line = "";          
             List<String> carPlates = new ArrayList<>();
+            
             int i = 0;
             LocalDateTime date = LocalDateTime.now();
             int currentMonth = date.getMonthValue();
-            
 
-            br.readLine();//skip first line
+            br.readLine();
             while ((line = br.readLine()) != null) {
                 String[] sales = line.split(",");
                 String currentTimeDate = sales[1];
                 LocalDateTime dateCheck = LocalDateTime.parse(currentTimeDate, DateTimeFormatter.ISO_DATE_TIME);
                 int monthCheck = dateCheck.getMonthValue();
+                
                 if (monthCheck == currentMonth) {
                     carPlates.add(sales[2]);
                 }
             }
             br.close();
+            
             BufferedReader brvehicle = new BufferedReader(new FileReader("vehicle.csv"));
             brvehicle.readLine();
+            
             while((line = brvehicle.readLine()) != null) {
                 String[] row = line.split(",");
                 if(carPlates.contains(row[0])) {
@@ -45,11 +49,10 @@ public class ViewProfitMargin {
                 
             }
             brvehicle.close();
-            
-            
-            
+
             BufferedReader brEmployee = new BufferedReader(new FileReader("employee.csv"));
             line = brEmployee.readLine();
+            
             while((line = brEmployee.readLine()) != null)  {
                 String[] row = line.split(",");
  
@@ -58,20 +61,16 @@ public class ViewProfitMargin {
                 totalSalaryEmployee = totalSalaryEmployee + salary.totalSalary();
             }
             brEmployee.close();
- 
- 
- 
+
         } catch (IOException | NumberFormatException e){
+            System.out.println("An error occured while calculating company profit margin.");
             e.printStackTrace();  
         }
  
-        // System.out.println(totalSalaryEmployee);
         double tax = (totalSalesPrice - totalAquirePrice - totalSalaryEmployee) * 0.10;
         double rent = 1000;
         double utility = 100;
-        return totalSalesPrice - totalAquirePrice - totalSalaryEmployee - rent - utility;
+        
+        return totalSalesPrice - totalAquirePrice - totalSalaryEmployee - rent - utility - tax;
     }
-    
-
-    
 }
