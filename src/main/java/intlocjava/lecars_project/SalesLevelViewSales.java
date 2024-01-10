528197624c53135c6f52a47d12fb7b78e2bbd73b
aiminoon <149665483+aiminoon@users.noreply.github.com>
2024-01-10 21:10:59 +0800

    Update SalesLevelViewSales.java

--- src/main/java/intlocjava/lecars_project/SalesLevelViewSales.java
+++ src/main/java/intlocjava/lecars_project/SalesLevelViewSales.java
@@ -5,43 +5,44 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-/**
- *
- * @author User
- */
+
 public class SalesLevelViewSales {
+    
     public static void displaySales(String empID){
         List<String[]> salesData = new ArrayList<>();
         
         try {
             BufferedReader br = new BufferedReader(new FileReader("sales.csv"));
             String line;
+            
             while((line = br.readLine()) != null)  {
-                String[] row = line.split(","); //split CSV line by comma
+                String[] row = line.split(","); 
                 salesData.add(row);
             }
-            
-            // Display sales records for the given employee ID
+
             System.out.println("Sales records for Employee ID " + empID + ":");
-            System.out.printf("%-10s   %-30s    %-14s    %-15s\n", "Sales ID", "Date", "Car Plate", "Customer ID", "Employee ID");
+            System.out.printf("%-10s   %-30s    %-14s    %-15s\n", 
+                    "Sales ID", "Date", "Car Plate", "Customer ID", "Employee ID");
+            
             for (String[] row : salesData){
-                if(row.length >= 5) { //ensure there are at least 5 elements in row array
+                if(row.length >= 5) { 
                     String salesId= row[0];
                     String dateTime = row[1];
                     String carPlate = row[2];
                     String custId = row[3];
                     String employeeId = row[4];
-             
                 
                     if(employeeId.equals(empID)){
-                        System.out.printf("%-10s   %-30s    %-14s    %-15s\n", salesId, dateTime, carPlate, custId, employeeId);
+                        System.out.printf("%-10s   %-30s    %-14s    %-15s\n", 
+                                salesId, dateTime, carPlate, custId, employeeId);
                     }
                 } else {
-                    // Handle rows with null data 
                     System.out.println("Invalid row format: " + String.join(", ", row));
                 }
             }
+            
         } catch (IOException | NumberFormatException e){
+            System.out.println("An error occured when printing data from sales.csv file.");
             e.printStackTrace();  
         }
     }
