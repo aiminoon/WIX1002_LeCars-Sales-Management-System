26be99b25b60b36479ea1261d65ce3f56dededaf
aiminoon <149665483+aiminoon@users.noreply.github.com>
2024-01-10 21:36:11 +0800

    Update ImportSalesData.java

--- src/main/java/intlocjava/lecars_project/ImportSalesData.java
+++ src/main/java/intlocjava/lecars_project/ImportSalesData.java
@@ -1,72 +1,14 @@
 package intlocjava.lecars_project;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-
-
-class Sales {
-    
-    private String salesId;
-    private String dateTime;
-    private String carPlate;
-    private String custId;
-    private String employeeId;
-    
-    public Sales(String sales, String date, String plate, String cust, String employ){
-        this.salesId = sales;
-        this.dateTime = date;
-        this.carPlate = plate;
-        this.custId = cust;
-        this.employeeId = employ;
-    }
-    
-    public String getSalesId() {
-        return salesId;
-    }
-    
-    public String getDate() {
-        return dateTime;
-    }
-    
-    public String getCarPlate() {
-        return carPlate;
-    }
-    
-    public String getCustId() {
-        return custId;
-    }
-    
-    public String getEmployeeId() {
-        return employeeId; 
-    }
-    
-    @Override
-    public String toString(){
-        return "Sales ID: " + salesId + ", Date: " + dateTime + ", Car Plate: " + carPlate + ", Customer Id: " + custId + ", Employee Id: " + employeeId; 
-    }
-
-    /**
-     * @return
-     */
-    public String toCSVFormat() {
-        return salesId + "," + dateTime + "," + carPlate + "," + custId + "," + employeeId;
-    }
-
-}
 
 public class ImportSalesData {
-    public static void main(String[] args) {
-        ImportSalesData.importSales();
-    }
-    
-    private static List<Sales> sales = new ArrayList<>();
 
     public static String getFilePath(){
         String currentDir = System.getProperty("user.dir");
+        
         return currentDir + File.separator + "sales.csv";
     }
-
     
     public static void importSales() {
         System.out.println(getFilePath()); 
@@ -78,20 +20,17 @@ public class ImportSalesData {
              BufferedWriter bw = new BufferedWriter(new FileWriter("sales.csv"))) {
 
             String line;
+            
             while ((line = br.readLine()) != null) {
-                String[] data = line.split(",");
-                if (data.length == 5) {
-                    Sales sale = new Sales(data[0], data[1], data[2], data[3], data[4]);
-                    // Assuming you have a method in Sales to get CSV representation
-                    String csvRepresentation = sale.toCSVFormat();
-                    bw.write(csvRepresentation);
-                    bw.newLine();
-                } else {
-                    System.out.println("Invalid line format: " + line);
-                }
+                bw.write(line);
+                bw.newLine(); 
             }
 
+            br.close();
+            bw.close();
+            
         } catch (IOException e) {
+            System.out.println("An error occured when importing sales data from sales.csv");
             e.printStackTrace();
         }
     } 
