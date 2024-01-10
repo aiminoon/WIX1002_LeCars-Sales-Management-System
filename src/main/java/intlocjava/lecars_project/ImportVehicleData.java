9f7f2c00b3e9c84308b3270cb32006b796718348
aiminoon <149665483+aiminoon@users.noreply.github.com>
2024-01-10 23:55:08 +0800

    Update ImportVehicleData.java

--- src/main/java/intlocjava/lecars_project/ImportVehicleData.java
+++ src/main/java/intlocjava/lecars_project/ImportVehicleData.java
@@ -28,6 +28,17 @@ public class ImportVehicleData {
             
             br.close();
             bw.close();
+
+            try (LineNumberReader lnr = new LineNumberReader(new FileReader("vehicle.csv"))) {
+                lnr.skip(Long.MAX_VALUE);
+                int rowsCount = -1;
+                rowsCount = lnr.getLineNumber();
+                System.out.println("Number of rows in vehicle.csv file is " + rowsCount);
+                
+            } catch (IOException e) {
+                System.out.println("An error occured while counting number of rows in vehicle.csv file.");
+                e.printStackTrace();
+            }
             
         } catch (IOException e) {
             System.out.println("An error occured when importing vehicle data from vehicle.csv");
